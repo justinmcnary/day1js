df714742934dd76a6a5585b212fd83f3791b6836
brianmsj <brianmsj@gmail.com>
2017-01-30 11:09:08 -0800

    added yearOfBirth function

--- day1js.js
+++ day1js.js
@@ -3,8 +3,12 @@
 function whoAmI (name,age){
   console.log ("Hi, my, name is " + name + " and I am " + age + " years old");
 
-  var yearOfBirth = 2017 - age;
+  var yob = yearOfBirth(age);
 
   console.log ("I was born in " + yearOfBirth);
 }
-console.log(whoAmI("Justin", 33 ));
+whoAmI();
+
+function yearOfBirth(age){
+
+}
