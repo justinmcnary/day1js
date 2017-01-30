4e0ade9fb465c673282d05536d49b01dd53252b6
justinmcnary <justinmcnary@yahoo.com>
2017-01-30 11:42:52 -0800

    nan prob

--- day1js.js
+++ day1js.js
@@ -6,13 +6,17 @@ function whoAmI (name,age){
 
   console.log ("I was born in " + yob);
 }
-whoAmI("Brian",-2);
+whoAmI("Brian", "34");
 
 function yearOfBirth(age){
   if (age < 0) {
     throw new Error ("Age cannot be a negative number");
 
   }
+  else if (isNaN(age)) {
+    throw new Error ("Arguments not valid");
+  }
+
   else {
   return 2016 - age;
   }
