23a5dedda0d21bdad3fffd9dcc7a02b9fe8434b2
brianmsj <brianmsj@gmail.com>
2017-01-30 11:26:45 -0800

    added if, else and error

--- day1js.js
+++ day1js.js
@@ -1,14 +1,19 @@
 
-
 function whoAmI (name,age){
   console.log ("Hi, my, name is " + name + " and I am " + age + " years old");
 
   var yob = yearOfBirth(age);
 
-  console.log ("I was born in " + yearOfBirth);
+  console.log ("I was born in " + yob);
 }
-whoAmI();
+whoAmI("Brian",-2);
 
 function yearOfBirth(age){
+  if (age < 0) {
+    throw new Error ("Age cannot be a negative number");
 
+  }
+  else {
+  return 2016 - age;
+  }
 }
